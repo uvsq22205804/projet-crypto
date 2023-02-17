075480ffaaaea1db64d7a576dcb64fb68febd99f
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-02-17 10:52:30 +0100

    update emma

--- projet_crypto.py
+++ projet_crypto.py
@@ -6,7 +6,7 @@
 # Déclaration des fonctions
 # - - - - - - - - - - - - -
 
-""" Code de César """
+""" Code de César : db --> string indices must be integers"""
 # alpha : alphabet
 
 def chiffrement(alpha, texte, decalage):
@@ -14,12 +14,13 @@ def chiffrement(alpha, texte, decalage):
         for j in alpha:  # j : variable contenant l'indice de alpha
             if texte[i]==alpha[j]:
                 texte_chiffre = alpha[j + decalage]
+
     return texte_chiffre
 
 def dechiffrement(alpha, texte, decalage):
     for i in texte:  # i : variable contenant l'indice de texte
         for j in alpha:  # j : variable contenant l'indice de alpha
-            if texte[i]==alpha[j]:
+            if texte[i] == alpha[j]:
                 texte_dechiffre = alpha[j - decalage]
 
     return texte_dechiffre
@@ -29,16 +30,17 @@ def dechiffrement(alpha, texte, decalage):
 # réutiliser fonction chiffrement du code de césar
 # on utilise toujours le même déclage dans le tableau du chiffre de Vigenère 
 
+"""Scytale"""
+
+"""substitution monoalphabetique générale"""
+
 
 # - - - - - - - - - -
 # Programme principal
 # - - - - - - - - - -
 
-# soit dictionnaire où on va aller chercher à quoi correspond chaque lettre, soit une liste avec tout
-# l'alphabet et pour récupérer la bonne lettre on décale l'indice de 1
-"""alphabet = {"a": "d", "b": "e", "c": "f", "d": "g", "e": "h", "f":"i", "g": "j","h": "k", "i": "l",
-            "j": "m", "k": "n", "l": "o", "m": "p", "n": "q", "o": "r", "p": "s","q": "t", "r": "u",
-            "s": "v", "t": "w", "u": "x" ,"v": "y", "w": "z", "x": "a", "y": "b", "z":"c",}"""
+# une liste avec alphabet et on pourra choisir le décalage
+
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 mon_texte = "totoparapluie"
