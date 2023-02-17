32a8146d2b80ba15619b1d911845b4a01dfc75f0
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-02-17 10:36:00 +0100

    update emma

--- projet_crypto.py
+++ projet_crypto.py
@@ -1,5 +1,6 @@
 # Emma : code de césar + scytale
-# Sawsane : chiffre de vigenère + une substitution monoalphabétique générale
+# Sawsane : chiffre de vigenère 
+# Iliane : une substitution monoalphabétique générale
 
 # - - - - - - - - - - - - -
 # Déclaration des fonctions
@@ -8,15 +9,20 @@
 """ Code de César """
 # alpha : alphabet
 
-def chiffrement(alpha, texte):
+def chiffrement(alpha, texte, decalage):
     for i in texte:  # i : variable contenant l'indice de texte
         for j in alpha:  # j : variable contenant l'indice de alpha
             if texte[i]==alpha[j]:
-                texte_chiffre = alpha[j + 3]
+                texte_chiffre = alpha[j + decalage]
+    return texte_chiffre
 
+def dechiffrement(alpha, texte, decalage):
+    for i in texte:  # i : variable contenant l'indice de texte
+        for j in alpha:  # j : variable contenant l'indice de alpha
+            if texte[i]==alpha[j]:
+                texte_dechiffre = alpha[j - decalage]
 
-def dechiffrement():
-    pass
+    return texte_dechiffre
 
 
 """ Chiffre de Vigenère """
@@ -36,8 +42,11 @@ def dechiffrement():
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 mon_texte = "totoparapluie"
+decalage = 3
 # texte = input("Quel est votre message: ")
+# decalage = int(input("Quel sera le décalage : "))
 mon_texte.lower()  # met tout le texte en minuscule pour ne pas avoir de problème avec les majuscules
 print(chiffrement(alphabet, mon_texte))
+print("finish")
 
 
