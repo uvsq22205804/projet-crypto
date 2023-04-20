0614e62e59e49768d7769facf0b7f628a50f15c8
enaili <113376502+uvsq22204044@users.noreply.github.com>
2023-04-20 23:42:43 +0200

    Update projet_crypto.py

--- projet_crypto.py
+++ projet_crypto.py
@@ -38,7 +38,43 @@ def dechiffrement_cesar(texte, key, alphabet):
 """Scytale"""
 
 """substitution monoalphabetique générale"""
+def substitution_cle(alphabet):  # fonction qui permets de générer une clé de substitution aléatoire
+    substitution_key=list(alphabet)  # crée une copie de la liste d'origine pour servir de clé de substitution
+    random.shuffle(substitution_key)  # mélange la liste pour obtenir une clé aléatoire
+    res = ""  # créé une chaîne vide pour stocker la clé de substitution
+    for char in substitution_key:  # parcourt chaque caractère dans la clé de substitution
+        res += char  # ajoute le caractère à la chaîne de résultat
+    return res  # renvoie la clé de substitution sous forme de chaîne de caractères
 
+key = substitution_cle(alphabet)
+ 
+
+def encrypt(lignes, substitution_key):  # fonction qui permets de crypter le message d'origine
+    encrypted_message = ""  # variable vide ou sera stocker le message crypté
+    for char in lignes:  # on parcourt tout les caractères du message d'origine
+        if char.lower() in alphabet:  # si le caractére fait partit de l'alphabet et si il est en minuscule
+            index = alphabet.index(char.lower())  # récupère l'index du caractère dans la liste alphabet
+            if char.isupper():   # si le caractére est en majuscule
+                encrypted_message += substitution_key[index%len(substitution_key)].upper()  # ajoute la lettre de substitution correspondante en majuscule
+            else:  # sinon, le caractère est en minuscule
+                encrypted_message += substitution_key[index%len(substitution_key)]  # ajoute la lettre de substitution correspondante en minuscule
+        else:  # si le caractère n'est pas alphabétique,
+            encrypted_message += char  # ajoute le caractère original tel quel
+    return encrypted_message  # renvoie le message chiffré
+
+
+def decrypt(lignes, key):
+    decrypted_message = ""  # initialise une chaîne vide pour stocker le message déchiffré
+    for char in lignes:  # parcourt chaque caractère dans le message chiffré
+        if char.lower() in key:  # vérifie si le caractère est alphabétique (en minuscule) et dans la clé
+            index = key.index(char.lower())  # récupère l'index du caractère dans la clé de substitution
+            if char.isupper():  # si le caractère est en majuscule,
+                decrypted_message += alphabet[index].upper()  # ajoute la lettre d'origine correspondante en majuscule                                                  
+            else:  # sinon, le caractère est en minuscule
+                decrypted_message += alphabet[index]  # ajoute la lettre d'origine correspondante en minuscule
+        else:  # si le caractère n'est pas alphabétique ou n'est pas dans la clé de substitution,
+            decrypted_message += char  # ajoute le caractère original tel quel
+    return decrypted_message  # renvoie le message déchiffré
 
 # - - - - - - - - - -
 # Programme principal
@@ -55,6 +91,17 @@ print("Le message chiffré est :", message_chiffre)
 message_dechiffre = dechiffrement_cesar(message_chiffre, cle, alphabet)
 print("Le message déchiffré est:", message_dechiffre)
 
+
+# ---print substitution---
+print("Message en clair :", messsage)
+
+encrypted_message = encrypt(message, key)
+print("Message chiffré :", encrypted_message)
+
+decrypted_message = decrypt(encrypted_message, key)
+print("Message déchiffré :", decrypted_message)
+
+print(cle)
 # ----- 
 
 
